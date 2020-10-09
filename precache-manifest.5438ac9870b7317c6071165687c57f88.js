e992eb0d3fadcf7153584295ecf631a7633d4360
ms0223900 <ms0223900@gmail.com>
2020-10-09 23:31:48 +0800

    Updates

---
+++ precache-manifest.5438ac9870b7317c6071165687c57f88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfa898359107932121136d1983a84bd",
+    "url": "/test-battle/index.html"
+  },
+  {
+    "revision": "2ab08a09b13a0da07179",
+    "url": "/test-battle/static/css/main.1fb46e0a.chunk.css"
+  },
+  {
+    "revision": "87d0e4f11087b1a181b6",
+    "url": "/test-battle/static/js/2.f237d935.chunk.js"
+  },
+  {
+    "revision": "d616de205072849b11cb448722b480a1",
+    "url": "/test-battle/static/js/2.f237d935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ab08a09b13a0da07179",
+    "url": "/test-battle/static/js/main.45ad0439.chunk.js"
+  },
+  {
+    "revision": "0cc9810a6bab18cd4802",
+    "url": "/test-battle/static/js/runtime-main.28b405ea.js"
+  }
+]);
